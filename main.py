4a23c62e308fb7d7dfa6391d406071e77238cb68
MathewIsBack <oseghaleokononfua@gmail.com>
2026-06-06 01:06:28 +0100

    new commit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 import time
 from emailing import send_email
 import glob
+import os
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
@@ -9,6 +10,11 @@ time.sleep(1)
 first_frame = None
 status_list = []
 count = 1
+
+def clean_folder():
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
 
 while True:
     status = 0
@@ -50,7 +56,8 @@ while True:
     status_list = status_list[-2:]
     
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email(image_with_object)      
+        send_email(image_with_object) 
+        clean_folder()     
         
     print(status_list)
     
